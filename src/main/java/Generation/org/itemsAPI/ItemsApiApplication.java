9aaadbaedb3c7a57891077f1b05a4c3364aad306
Kimberly MB <k.mueses@yahoo.com>
2022-12-12 10:53:43 -0500

    Generates Spring Boot

---
+++ src/main/java/Generation/org/itemsAPI/ItemsApiApplication.java
@@ -0,0 +1,13 @@
+package Generation.org.itemsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Generation/org/itemsAPI/ItemsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Generation.org.itemsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
